d2608fcb79b78934acacfa3cb73763c71275820e
Sean Watkins <seanlw@gmail.com>
2012-03-31 17:41:04 -0500

    Updated to support Drupal Coding Standards

--- libhours-display.tpl.php
+++ libhours-display.tpl.php
@@ -22,9 +22,9 @@
 	<div id="libhours-locations">
 		<ul id="libhours-location-list">
 			<?php foreach($locations as $location): ?>
-				<li class="libhours-location <?php echo (($location['lid'] == $lid)?'selected':'')?>" onclick="javascript:libhours.location('<?php echo $location['lid'] ?>')"><?php echo $location['name']?></li>
+				<li class="libhours-location <?php echo (($location['lid'] == $lid) ? 'selected' : '') ?>" onclick="javascript:libhours.location('<?php echo $location['lid'] ?>')"><?php echo $location['name'] ?></li>
 				<?php foreach($location['children'] as $child): ?>
-					<li class="libhours-location libhours-child <?php echo (($child['lid'] == $lid)?'selected':'') ?>" onclick="javascript:libhours.location('<?php echo $child['lid'] ?>')">- <?php echo $child['name'] ?></li>
+					<li class="libhours-location libhours-child <?php echo (($child['lid'] == $lid) ? 'selected' : '') ?>" onclick="javascript:libhours.location('<?php echo $child['lid'] ?>')">- <?php echo $child['name'] ?></li>
 				<?php endforeach; ?>
 			<?php endforeach; ?>
 		</ul>
@@ -32,14 +32,14 @@
 	<div id="libhours-periods">
 		<ul id="libhours-periods-tabs">
 			<?php foreach($periods as $period): ?>
-				<li id="libhours-tab-<?php echo $period['pid'] ?>" class="<?php echo (($period['pid'] == $pid)?'selected':'') ?>" onclick="javascript:libhours.tab('<?php echo $period['pid'] ?>')">
+				<li id="libhours-tab-<?php echo $period['pid'] ?>" class="<?php echo (($period['pid'] == $pid) ? 'selected' : '') ?>" onclick="javascript:libhours.tab('<?php echo $period['pid'] ?>')">
 					<div class="libhours-name"><?php echo $period['name'] ?></div>
 					<div class="libhours-daterange"><?php echo date("M j", $period['from_date']) ?> - <?php echo date("M j", $period['to_date']) ?></div>
 				</li>
 			<?php endforeach; ?>
 		</ul>
 		<?php foreach($periods as $period): ?>
-			<div id="libhours-period-<?php echo $period['pid'] ?>" class="libhours-view <?php echo (($period['pid'] == $pid)?'selected':'') ?>">
+			<div id="libhours-period-<?php echo $period['pid'] ?>" class="libhours-view <?php echo (($period['pid'] == $pid) ? 'selected' : '') ?>">
 				<div class="libhours-stdhours">
 					<div class="libhours-name"><?php echo $period['name'] ?> Hours</div>
 					<ul>
